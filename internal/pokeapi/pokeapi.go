aa7b7f207c66d2c1a3eef2024c0c9af205746054
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2023-10-07 19:53:57 +0300

    refactor: cache helper

--- internal/pokeapi/pokeapi.go
+++ internal/pokeapi/pokeapi.go
@@ -9,6 +9,16 @@ import (
 	"github.com/Pertsaa/pokedex-cli/internal/pokecache"
 )
 
+type PokeApi struct {
+	cache *pokecache.Cache
+}
+
+func New(cacheDuration time.Duration) *PokeApi {
+	return &PokeApi{
+		cache: pokecache.New(cacheDuration),
+	}
+}
+
 type LocationAreaResponse struct {
 	Count    int            `json:"count"`
 	Next     *string        `json:"next"`
@@ -21,47 +31,45 @@ type LocationArea struct {
 	URL  string `json:"url"`
 }
 
-type PokeApi struct {
-	cache *pokecache.Cache
-}
-
-func New(cacheDuration time.Duration) *PokeApi {
-	return &PokeApi{
-		cache: pokecache.New(cacheDuration),
-	}
-}
-
 func (p *PokeApi) GetLocationAreas(pageURL *string) (*LocationAreaResponse, error) {
 	url := "https://pokeapi.co/api/v2/location-area/"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
+	bytes, err := getWithCache(url, p.cache)
+	if err != nil {
+		return &LocationAreaResponse{}, err
+	}
+
+	var lr LocationAreaResponse
+	if err := json.Unmarshal(bytes, &lr); err != nil {
+		return &LocationAreaResponse{}, err
+	}
+
+	return &lr, nil
+}
+
+func getWithCache(url string, cache *pokecache.Cache) ([]byte, error) {
 	var bytes []byte
 
-	if cb, ok := p.cache.Get(url); ok {
+	if cb, ok := cache.Get(url); ok {
 		bytes = cb
 	} else {
 		resp, err := http.Get(url)
 		if err != nil {
-			return &LocationAreaResponse{}, err
+			return []byte{}, err
 		}
 
 		defer resp.Body.Close()
 
 		bytes, err = io.ReadAll(resp.Body)
 		if err != nil {
-			return &LocationAreaResponse{}, err
+			return []byte{}, err
 		}
 
-		p.cache.Add(url, bytes)
+		cache.Add(url, bytes)
 	}
 
-	var lr LocationAreaResponse
-	err := json.Unmarshal(bytes, &lr)
-	if err != nil {
-		return &LocationAreaResponse{}, err
-	}
-
-	return &lr, nil
+	return bytes, nil
 }
