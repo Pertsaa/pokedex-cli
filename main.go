252d31c6376cff743019506451441cdaca77111e
Pertsaa <65128229+Pertsaa@users.noreply.github.com>
2023-10-07 17:59:21 +0300

    feat: REPL

--- main.go
+++ main.go
@@ -1,9 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 func main() {
-	fmt.Println("Hello World")
+	commands := getCommands()
+
+	for {
+		fmt.Print("pokedex > ")
+
+		scanner := bufio.NewScanner(os.Stdin)
+
+		scanner.Scan()
+		command := scanner.Text()
+
+		if _, ok := commands[command]; !ok {
+			fmt.Printf("Command not found: %s\n", command)
+
+			continue
+		}
+
+		commands[command].callback()
+	}
+}
+
+func commandHelp() error {
+	fmt.Print("\nWelcome to the Pokedex!\n")
+	fmt.Print("\nUsage:\n")
+
+	commands := getCommands()
+
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
+
+	fmt.Println()
+
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
